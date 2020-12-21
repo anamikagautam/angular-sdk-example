1301c6268d778741f13c1ad696f8dcda7255363f
roshanchaube <chauberoshan@gmail.com>
2020-12-21 13:08:57 +0000

    update

--- angular-sdk-example-backend/src/main/java/org/ehrbase/angularsdkexample/controller/DiagnosisController.java
+++ angular-sdk-example-backend/src/main/java/org/ehrbase/angularsdkexample/controller/DiagnosisController.java
@@ -105,7 +105,7 @@ public class DiagnosisController {
 
             content = content.replaceAll("anamika", UUID.randomUUID().toString());
             content = content.replaceAll("apnatimeaega", result1.toString());
-             File tempFile = new File("OutputFile");
+             File tempFile = new File("/workspace/angular-sdk-example/OutputFile.json");
              FileUtils.writeStringToFile(tempFile, content, "UTF-8");
             System.out.println(tempFile);
         } catch (IOException e) {
@@ -125,11 +125,7 @@ public class DiagnosisController {
        // String command2 = "1d1d4428-c475-4158-a1df-a69781c81c0c";
        // String command3 = "/compositions?auditCommitter=Sumit%20House,%20MD.";
        // String command = command1+command2+command3;
-        String command = "curl -X POST --header"+
-        "Content-Type:application/json"+
-        "--header"+ "Authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQGNhYm9sYWJzLmNvbSIsImV4dHJhZGF0YSI6eyJvcmdfdWlkIjoiZTlkMTMyOTQtYmNlNy00NGU3LTk2MzUtOGU5MDZkYTBjOTE0In0sImlzc3VlZF9hdCI6IjIwMjAtMTItMjFUMTA6Mzg6MTYuNTc0WiIsImV4cGlyZXNfYXQiOiIyMDIwLTEyLTIyVDEwOjM4OjE2LjU3NVoifQ.01rMbHq1y998HEUK_oUr4XN6U9oomLFUdBkhn1oPXWM" +
-        "--data"+ "@/workspace/angular-sdk-example/angular-sdk-example-backend/OutpuFile"+
-        " http://35.198.226.194:8090/rest/v1/ehrs/"+"1d1d4428-c475-4158-a1df-a69781c81c0c"+"/compositions?auditCommitter=Sumit%20House,%20MD.";
+        String command = "curl -X POST --header \"Content-Type:application/json\" --header \"Authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQGNhYm9sYWJzLmNvbSIsImV4dHJhZGF0YSI6eyJvcmdfdWlkIjoiZTlkMTMyOTQtYmNlNy00NGU3LTk2MzUtOGU5MDZkYTBjOTE0In0sImlzc3VlZF9hdCI6IjIwMjAtMTItMjFUMTA6Mzg6MTYuNTc0WiIsImV4cGlyZXNfYXQiOiIyMDIwLTEyLTIyVDEwOjM4OjE2LjU3NVoifQ.01rMbHq1y998HEUK_oUr4XN6U9oomLFUdBkhn1oPXWM\" --data @OutputFile.json http://35.198.226.194:8090/rest/v1/ehrs/1d1d4428-c475-4158-a1df-a69781c81c0c/compositions?auditCommitter=Sumit%20House,%20MD.";
         System.out.println(command);
         try {
             Process process = Runtime.getRuntime().exec(command);
